29fd5267a194c6b654825f8ed18925d88ac24f03
Harnish Mistry <harrnish@gmail.com>
2024-03-06 22:58:53 -0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Codegrid</h1>
+      <p>Forging ahead with elite web designs, block by block.</p>
+    </>
+  );
+}
+
+export default App;
