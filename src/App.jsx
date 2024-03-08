bf16afcdf3132fff7411644c768c3663dc8c6f29
Harnish Mistry <harrnish@gmail.com>
2024-03-07 23:33:59 -0500

    intro text

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
 import "./App.css";
+
+import DisplayPicture from "./assets/images/harrnish.png";
 
 function App() {
   return (
     <>
       <div className="container">
-        <h1>
-          Forging ahead with <span>Elite</span> web designs.
-        </h1>
+        <div className="message">
+          <div className="message-id">
+            <img src={DisplayPicture} alt="" />
+          </div>
+          <div className="message-copy">
+            <p className="msg">
+              Hey there, I'm Harrnish, you're a total giga-chad 💛🗿! This
+              exclusive membership is just for chads like you, who want to make
+              elite websites and think time is super important.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
