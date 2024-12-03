9639c8f0887956dd3488606c86c5fd91ac8cb003
Harnish <harrnish@gmail.com>
2024-12-02 22:32:23 -0500

    landing page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+
+import { PiArrowUpRight } from "react-icons/pi";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Menu />
+
+      <div className="header">
+        <h1>Forging ahead with elite web designs</h1>
+
+        <button>
+          Subscribe
+          <div className="btn-icon">
+            <PiArrowUpRight />
+          </div>
+        </button>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="mono">Watch Showreel</p>
+
+      <p className="mono">Get PRO</p>
+    </div>
+  );
+};
+
+export default Footer;
